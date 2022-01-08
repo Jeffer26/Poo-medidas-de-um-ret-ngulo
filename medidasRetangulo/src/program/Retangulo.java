0407145f1b50ede3bfbe60d1b34f0cbf039ceeca
Jefferson da Silva Vieira <jsv@hotmail.com.br>
2022-01-08 19:54:57 -0300

    Acrescentado a frase digite o valor da area

--- medidasRetangulo/src/program/Retangulo.java
+++ medidasRetangulo/src/program/Retangulo.java
@@ -10,6 +10,7 @@ public class Retangulo {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o valor da area: ");
 		Area x;
 		x = new Area();
 		
